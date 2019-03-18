b8552c94f580134d4df788c43ca25077c47c5a28
Jouny Enzo <enzo@macmini.local>
2019-03-18 11:41:09 +0100

    Ajout des nouveaux Contrôleurs

---
+++ Les Calanques/Modèle/Calanques.swift
@@ -0,0 +1,44 @@
+//
+//  Calanques.swift
+//  Les Calanques
+//
+//  Created by Jouny Enzo on 18/03/2019.
+//  Copyright © 2019 JounyCorp. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class Calanque {
+
+    private var _nom: String
+    private var _desc: String
+    private var _latitude: Double
+    private var _longitude: Double
+    private var _image: UIImage?
+
+    var nom: String {
+        return _nom
+    }
+
+    var desc: String {
+        return _desc
+    }
+
+    var coordonnee: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: _latitude, longitude: _longitude)
+    }
+
+    var image: UIImage? {
+        return _image
+    }
+    
+    init(nom: String, desc: String, image: UIImage?, latitude: Double, longitude: Double) {
+        _nom = nom
+        _desc = desc
+        _image = image
+        _latitude = latitude
+        _longitude = longitude
+    }
+    
+}
