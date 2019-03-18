4377638061a3b7861c4fed06943be63fe95bd47e
Jouny Enzo <enzo@macmini.local>
2019-03-18 13:54:48 +0100

    Logique UITableView delegate

--- Les Calanques/Controlleur/CoontrollerAvecTableView.swift
+++ Les Calanques/Controlleur/CoontrollerAvecTableView.swift
@@ -8,15 +8,41 @@
 
 import UIKit
 
-class CoontrollerAvecTableView: UIViewController {
+class CoontrollerAvecTableView: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var TableView: UITableView!
+  
+    var calanques: [Calanque] = []
+    var cellId = "Alternative"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        TableView.delegate = self
+        TableView.dataSource = self
+        calanques = CalanqueCollection().all()
         // Do any additional setup after loading the view.
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return calanques.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let calanque = calanques[indexPath.row]
+        if let cell = tableView.dequeueReusableCell(withIdentifier: cellId)
+        as? CalanqueCellAlternative {
+        cell.calanque = calanque
+        return cell
+    }
+    return UITableViewCell()
+    }
 
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
+    
+    
     /*
     // MARK: - Navigation
 
